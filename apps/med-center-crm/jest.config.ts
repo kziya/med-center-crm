b7c0db7b51710a25f7d4a7f2e68e676d69357795
Ziya Karimli <ziyakerimli04@gmail.com>
2025-04-29 21:20:37 +0300

    Initial commit

---
+++ apps/med-center-crm/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'med-center-crm',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/med-center-crm'
+};
