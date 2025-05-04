fabb5ce4caf969a3ad0eba570d6e1e4625ae0782
Ziya Karimli <ziyakerimli04@gmail.com>
2025-05-04 18:17:28 +0300

    add: Common typeorm config

--- apps/auth-service/src/app/auth/auth.module.ts
+++ apps/auth-service/src/app/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { CommonAuthModule } from '@med-center-crm/auth';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [CommonAuthModule],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
---
+++ libs/common/src/lib/configs/typeorm.config.ts
@@ -0,0 +1,39 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import {
+  ActivityLogs,
+  AppointmentDetails,
+  Appointments,
+  DoctorDetails,
+  DoctorPatientAssignment,
+  DoctorReviews,
+  PatientDetails,
+  PatientLabResults,
+  UserContacts,
+  Users,
+} from '@med-center-crm/types';
+
+export const TypeormConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    driver: require('pg'),
+    url: configService.get('POSTGRES_URL'),
+    synchronize: false,
+    type: 'postgres',
+    entities: [
+      ActivityLogs,
+      Appointments,
+      AppointmentDetails,
+      DoctorDetails,
+      DoctorPatientAssignment,
+      DoctorReviews,
+      PatientDetails,
+      PatientLabResults,
+      Users,
+      UserContacts,
+    ],
+    bigNumberStrings: false,
+  }),
+  inject: [ConfigService],
+};
